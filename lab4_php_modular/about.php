eb2b390bdd34b49bab2cceb271ed4bf07b1dd8cd
Basrisngji <basrisangaji23@gmail.com>
2023-04-03 17:44:16 +0700

    Menambahkan File

---
+++ lab4_php_modular/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>About</h2>
+    <div class="box">
+        <p> Terimakasih sudah mengunjungi website kami,semoga anda puas dengan pelayanan yang kami berikans.</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Basri Sangaji</p>
+        <p>Nim : 312110152</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : basrisangaji23@gmail.com</p>
+        <p>No. Telp : 08783452224</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
